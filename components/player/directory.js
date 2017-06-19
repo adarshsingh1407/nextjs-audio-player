b650a903e1bf1617ea212da905d14cf875b06141
Adarsh Singh <adarsh.singh@abcplusd.com>
2017-06-19 14:49:05 +0530

    Songs source changed

--- components/player/directory.js
+++ components/player/directory.js
@@ -3,35 +3,35 @@ export const songsDirectory = [
     "id": 1,
     "name": "Remember Us",
     "artist": "The Pineapple Thief",
-    "src": "/static/remember-us.mp3",
+    "src": "http://mp3.progarchives.com/StreamMP3.asp?id=169",
     "img": "https://i.ytimg.com/vi/PWzmVEn0M3U/hqdefault.jpg"
   },
   {
     "id": 2,
     "name": "Trains",
     "artist": "Porcupine Tree",
-    "src": "/static/trains.mp3",
+    "src": "http://a.tumblr.com/tumblr_m5e9tgdyhQ1qztmnoo1.mp3",
     "img": "https://i.ytimg.com/vi/0UHwkfhwjsk/hqdefault.jpg"
   },
   {
     "id": 3,
     "name": "Crazy",
     "artist": "Aerosmith",
-    "src": "/static/crazy.mp3",
+    "src": "http://api.ning.com/files/juXM4C5W7ayCvfVh8kvBS4ppzh6idFe7-xo0ALVY9Q0blAw*g36-rq3ZNgtUHwtEYtm0MirBCw4NBUnDsG2wXQOukQ0r7C1E/nKKYoKRsoKKgfayOoZeOjqNoa2c3.mp3",
     "img": "https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/GetAGrip_Aerosmithalbum.jpg/220px-GetAGrip_Aerosmithalbum.jpg"
   },
   {
     "id": 4,
     "name": "Hey Jude",
     "artist": "The Beatles",
-    "src": "/static/hey-jude.mp3",
+    "src": "http://markmizzles.whyprime.com/newmusic/VA%20-%20Across%20The%20Universe%20Soundtrack/15-joe_anderson-hey_jude.mp3",
     "img": "https://i.ytimg.com/vi/t5c-LVYnafg/0.jpg"
   },
   {
     "id": 5,
     "name": "Wonderwall",
     "artist": "Oasis",
-    "src": "/static/wonderwall.mp3",
+    "src": "http://api.ning.com/files/u-ZDkx7q9ectCMQ59fUYW6yjq79hodp86LHxgGm9UeBW41QsP6Ge*1IgPP*nR0RXQhG11XbTcpFFgHw2*vY3weaPGfwtf0ua/gJOmnaiNpqaVl6WrlqKjOA$$.mp3",
     "img": "http://www.oasisinet.com/wp-content/uploads/2013/11/facebook-logo.jpg"
   }
 ]
